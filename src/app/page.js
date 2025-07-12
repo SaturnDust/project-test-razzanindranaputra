385c6880fba3e223ccbfce6462c2307343496424
SaturnDust <razzanindrana26@gmail.com>
2025-07-12 23:06:09 +0700

    Wrap PostIdeas component in Suspense for improved loading state handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Header from '@/ui/header.jsx';
+import { Suspense } from "react";
 import PostIdeas from '@/ui/postIdeas.jsx';
 
 export default function Home() {
   return (
       <> 
         <Header />
-        <PostIdeas />
+        <Suspense fallback={<div>Loading post...</div>}>
+          <PostIdeas />
+        </Suspense>
       </>
   );
 }
